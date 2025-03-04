e29000a983ceec4e1cb1986c9658864595381529
Dovlet Begenjev <dovlet.begenjev.99@gmail.com>
2025-03-04 15:05:11 +0500

    Made elements adjustable to screen size

--- WebApplication2/obj/Debug/net9.0/WebApplication2.AssemblyInfo.cs
+++ WebApplication2/obj/Debug/net9.0/WebApplication2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bef216770d390c30a2722223521c266c5a6c534b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
